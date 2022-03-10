8485ce50df1d2b7692168bcdd397db355fb1b1c6
BoluwatifeO <boluwatifeoyetoro@gmail.com>
2022-03-10 19:33:17 +0100

    adding starting project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import "./Images/reactjs-icon.png";;
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
